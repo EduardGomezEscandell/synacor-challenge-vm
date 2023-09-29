7651f8eac3b38f86e8c5304e4132135f42e19d43
Edu Gómez Escandell <eduard.gomez.escandell@gmail.com>
2023-09-30 01:32:32 +0200

    Run fixture generator in QA

--- vm/test/testdata/fixtures/assemble-all.sh
+++ vm/test/testdata/fixtures/assemble-all.sh
@@ -1,10 +1,12 @@
 cd $(dirname $0)
 sources=$(find . -regex '.*\.as')
 
+BUILD_TYPE=${BUILD_TYPE:-Release}
+
 for src in $sources ; do
     dest=$(echo "$src" | sed 's#^\(.*\)\.as$#\1.in#')
-    ../../../../build/Release/assembler/cmd/assemble "$src" "$dest"     \
-        && echo "Built $dest"                                           \
-        || echo "Could not build $dest" >&2
+    "../../../../build/${BUILD_TYPE}/assembler/cmd/assemble" "$src" "$dest"     \
+                && echo "Built $dest"                                           \
+                || echo "Could not build $dest" >&2
 done
 cd - > /dev/null
